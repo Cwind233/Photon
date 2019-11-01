9ff1862855b0b29f9d55190fa406fe8bf848344b
Cwind233 <657928256@qq.com>
2019-11-01 19:52:00 +0800

    19:51:54 2019年11月1日

--- Photon/src/util/Photon.sql
+++ Photon/src/util/Photon.sql
@@ -21,27 +21,31 @@ USE `photon`;
 DROP TABLE IF EXISTS `login`;
 
 CREATE TABLE `login` (
-  `userName` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,--用户名
-  `password` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,--用户密码
+  `userName` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`userName`),
   CONSTRAINT `FK_login` FOREIGN KEY (`userName`) REFERENCES `personalinformation` (`userName`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `login` */
 
+insert  into `login`(`userName`,`password`) values ('098','098'),('123','123'),('321','321'),('345','345'),('876','876'),('987','987');
+
 /*Table structure for table `personalinformation` */
 
 DROP TABLE IF EXISTS `personalinformation`;
 
 CREATE TABLE `personalinformation` (
-  `userName` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,--用户名
-  `nickName` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,--用户昵称
-  `email` varchar(30) COLLATE utf8mb4_general_ci DEFAULT NULL,--用户邮箱
-  `headImage` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,--用户头像
-  PRIMARY KEY (`userName`)
+  `userName` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `nickName` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `headImage` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`userName`,`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `personalinformation` */
+
+insert  into `personalinformation`(`userName`,`nickName`,`email`,`headImage`) values ('098','test','test','test'),('123','123','123','123'),('321','321','321','321'),('345','test','test','test'),('876','test','test','test'),('987','test','test','test');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
