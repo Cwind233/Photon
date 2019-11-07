38b1d53c14188c071c454a1ebe76067fe44bbe4b
Cwind233 <657928256@qq.com>
2019-11-07 19:58:17 +0800

    20:59:59 2019年11月7日

--- Photon/src/servlet/LoginServlet.java
+++ Photon/src/servlet/LoginServlet.java
@@ -14,6 +14,7 @@ import org.apache.commons.collections.set.SynchronizedSortedSet;
 import org.json.JSONObject;
 
 import entity.User;
+import service.impl.CheckStringServiceImpl;
 import service.impl.UserServiceImpl;
 
 /**
@@ -22,28 +23,40 @@ import service.impl.UserServiceImpl;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	int flag = -1;
+	JSONObject json = new JSONObject();
+	String msg="";
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //	    request.setCharacterEncoding("utf-8");
-	    int flag = -1;
-	    //创建JSON对象
-	    JSONObject json=new  JSONObject();
-	    //创建输出流对象
+	    
+	    //创建输出流对象 设置编码
 	    response.setContentType("text/html;charset=utf-8");
 	    PrintWriter out=response.getWriter();
-	     String msg="";
+	     
         // 获取帐号密码
         String userName = request.getParameter("uname");
         String password = request.getParameter("password");
         String verify = request.getParameter("verify");
         String checkVerify = request.getParameter("checkVerify");
         
+        //输入正确返回 3
+        if(CheckStringServiceImpl.CheckLoginString(userName, password, verify) != 3) {
+            flag = 8;//输入错误
+            msg = "输入信息有误";
+            json.put("msg", msg);
+            json.put("flag",flag);
+            out.print(json);
+            return ;
+        }
+        
         //判断验证码
-        System.out.println(checkVerify);
-        System.out.println(verify);
+        System.out.println(checkVerify);//test
+        System.out.println(verify);//test
         if(!checkVerify.equals(verify)) {
-            flag = 9;
-            msg="验证码错误";
+            flag = 9;//验证码错误
+            msg = "验证码错误";
             json.put("msg", msg);
             json.put("flag",flag);
             out.print(json);
@@ -53,13 +66,11 @@ public class LoginServlet extends HttpServlet {
         
         //创建用户Dao对象
         User user = new User(userName, password);
-        user.setUserName(userName);
-        user.setPassword(password);
-        
         user.setEmail("test");//tests
         user.setNickName("test");//test
         user.setHeadImage("test");//test
-        System.out.println(user.getUserName() + "    "+ user.getPassword());
+        System.out.println(user.getUserName() + " \n   "+ user.getPassword());//test
+        
         //校验密码 返回flag  0失败 1成功 2不存在
         flag = new UserServiceImpl().queryUserPassword(user);
         System.out.println(flag);
@@ -70,13 +81,13 @@ public class LoginServlet extends HttpServlet {
 //            System.out.println("111");
 //            response.sendRedirect("login.html");
             System.out.println("密码错误");
-            msg="密码错误";
+            msg = "密码错误";
             json.put("msg", msg);
         }else if(flag == 1){//1可以
             //登录验证成功后，查询用户信息
             User queryUser = new User();
             queryUser = new UserServiceImpl().queryUserByUserName(user);
-            //写入session
+//            //写入session
 //            request.getSession().setAttribute("userName", queryUser.getUserName());
 //            request.getSession().setAttribute("nickName", queryUser.getNickName());
 //            request.getSession().setAttribute("headImage", queryUser.getHeadImage());
@@ -87,23 +98,20 @@ public class LoginServlet extends HttpServlet {
             json.put("userName", queryUser.getUserName());
             json.put("nickName", queryUser.getNickName());
             json.put("headImage", queryUser.getHeadImage());
-            
-            
         }else if(flag == 2) {
 //            request.getSession().setAttribute("message", "登录失败，用户不存在");
 //            response.sendRedirect("logintest.jsp");//test
 //            response.sendRedirect("login.html");
-            msg="登录失败，用户不存在";
+            msg = "登录失败，用户不存在";
             json.put("msg", msg);
         }else {
 //            request.getSession().setAttribute("message", "服务器异常，请重试");
 //            response.sendRedirect("logintest.jsp");//test
 //            response.sendRedirect("login.html");
-            msg="服务器异常，请重试";
+            msg = "服务器异常，请重试";
             json.put("msg", msg);
         }
-        System.out.println(json);
-        
+        System.out.println(json);//test
         out.print(json);
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- Photon/src/servlet/RegisterServlet.java
+++ Photon/src/servlet/RegisterServlet.java
@@ -12,25 +12,38 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONObject;
 
 import entity.User;
+import service.impl.CheckStringServiceImpl;
 import service.impl.UserServiceImpl;
 @WebServlet("/RegisterServlet")
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	String msg = "";
+	JSONObject json = new JSONObject(); 
+	int flag = -1;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //        request.setCharacterEncoding("utf-8");
-	    String msg="";
-	    JSONObject json=new JSONObject(); 
-	    int flag = -1;
+	    //创建输出流对象 设置编码
+	    response.setContentType("text/html;charset=utf-8");
 	    PrintWriter out=response.getWriter();
 	    
-        // 获取帐号密码  verify
+        // 获取表单信息
         String userName = request.getParameter("userName");
         String password = request.getParameter("password");
-        String verify= request.getParameter("verify");
-//        String nickName = request.getParameter("userName");                                                     
+//        String nickName = request.getParameter("nickName");                                                     
         String email = request.getParameter("email");
+        String verify= request.getParameter("verify");
         String checkVerify = request.getParameter("checkVerify");
+        
+        //校验用户输入文本  防止非法输入  输入正确返回 4
+        if(CheckStringServiceImpl.CheckRegisterString(userName, password, email, verify) != 4) {
+            flag = 8;//输入错误
+            msg = "输入信息有误";
+            json.put("msg", msg);
+            json.put("flag",flag);
+            out.print(json);
+            return ;
+        }
         
         //判断验证码
         if(!checkVerify.equals(verify)) {
@@ -42,36 +55,33 @@ public class RegisterServlet extends HttpServlet {
         
         //创建用户Dao对象
         User user = new User(userName, password);
-        user.setUserName(userName);
-        user.setPassword(password);
-        user.setEmail("test");
-        user.setNickName("test");
-        user.setHeadImage("test");
-        System.out.println(user.getUserName());
-        //写入session
-        //0失败 1成功 2已存在
+        user.setEmail("test");//test
+        user.setNickName("test");//test
+        user.setHeadImage("test");//test
+        System.out.println(user.getUserName());//test
+        //0失败 1成功 2已存在 else服务器错误
         flag = new UserServiceImpl().addUser(user);
         System.out.println(flag);//test
         json.put("flag", flag);
         if(flag == 0) {
 //            request.getSession().setAttribute("message", "注册失败，请重试");
 //            response.sendRedirect("register.jsp");
-            msg= "注册失败，请重试";
+            msg = "注册失败，请重试";
             json.put("msg",msg);
         }else if(flag == 1) {
 //            request.getSession().setAttribute("message", "注册成功");
 //            response.sendRedirect("login.jsp");
-            msg= "注册成功";
+            msg = "注册成功";
             json.put("msg",msg);
         }else if(flag == 2) {
 //            request.getSession().setAttribute("message", "注册失败，用户已存在");
 //            response.sendRedirect("register.jsp");
-            msg= "注册失败，用户已存在";
+            msg = "注册失败，用户已存在";
             json.put("msg",msg);
         }else {
 //            request.getSession().setAttribute("message", "服务器异常");
 //            response.sendRedirect("register.jsp");
-            msg= "服务器异常";
+            msg = "服务器异常";
             json.put("msg",msg);
         }
         out.print(json);
